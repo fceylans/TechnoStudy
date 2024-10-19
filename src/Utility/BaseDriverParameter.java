988deae3fdfc464b09374b5df6b350557bf19d9e
fceyl <fceylan0072@gmail.com>
2024-10-20 00:00:40 +0300

    Düzenleme

--- src/Utility/BaseDriverParameter.java
+++ src/Utility/BaseDriverParameter.java
@@ -1,7 +1,5 @@
 package Utility;
 
-
-
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -17,35 +15,38 @@ import org.testng.asserts.SoftAssert;
 import java.time.Duration;
 
 public class BaseDriverParameter {
-    public static SoftAssert softAssert=new SoftAssert();
+    public static SoftAssert softAssert = new SoftAssert();
     public WebDriver driver;
     public WebDriverWait wait;
     public static JavascriptExecutor js;
 
     @BeforeClass
-   @Parameters("browserTipi")
-    public void BaslangicIslemleri(String browserTipi){
-        // System.out.println("Başlangıç işlemleri yapılıyor");
+    @Parameters("browserTipi")
+    public void BaslangicIslemleri(String browserTipi) {
 
-        switch (browserTipi.toLowerCase())
-        {
-            case "firefox": driver=new FirefoxDriver();break;
-            case "edge": driver=new EdgeDriver();break;
-            case "safari": driver=new SafariDriver();break;
-            default: driver=new ChromeDriver();
+        switch (browserTipi.toLowerCase()) {
+            case "firefox":
+                driver = new FirefoxDriver();
+                break;
+            case "edge":
+                driver = new EdgeDriver();
+                break;
+            case "safari":
+                driver = new SafariDriver();
+                break;
+            default:
+                driver = new ChromeDriver();
         }
 
-        //driver.manage().window().maximize(); // Ekranı max yapıyor.
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20)); // 20 sn mühlet: sayfayı yükleme mühlet
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20)); // 20 sn mühlet: elementi bulma mühleti
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
         wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-        js=(JavascriptExecutor)driver;
+        js = (JavascriptExecutor) driver;
 
     }
 
     @AfterClass
-    public void KapanisIslemleri(){
-        // System.out.println("Kapanis işlemleri yapılıyor");
+    public void KapanisIslemleri() {
 
         Tools.Wait(3);
         driver.quit();
